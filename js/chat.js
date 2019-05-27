ad058001123069b506baa7235abcf16fcc63dd8f
Hain Lukas <if170041@WS8A13.local>
2019-05-27 08:48:32 +0200

    fixed usernameinput

--- js/chat.js
+++ js/chat.js
@@ -1,10 +1,8 @@
 window.onload = function() {
   let usernameInput = document.querySelector("#username-input")
   usernameInput.onkeydown = function(event) {
-    if (usernameInput.value.length > 10 ) {
-      if (!(usernameInput.value == "Backspace")) {
+    if (usernameInput.value.length > 10 && !(event.keyCode == 8) && !(event.keyCode == 37) && !(event.keyCode == 39)) {
         return false
-      }
     }
   }
 }
